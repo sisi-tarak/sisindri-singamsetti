83f05a188b3174b74b600e5fc008e0e44977c064
Sisindri Singamsetti <sisindrisingamsetti@gmail.com>
2024-12-21 17:13:12 +0530

    responsive for upside cover and projects section updated

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <div className="">
       <Container maxWidth="lg" className="py-12">
-        <Box className="px-8 xs:px-10 sm:px-12 md:px-20 lg:px-28 gap-x-9 gap-y-3 grid grid-flow-row grid-cols-1 sm:grid-cols-2 ">
+        <Box className="px-8 xs:px-24 sm:px-10 md:px-20 lg:px-28 gap-x-9 gap-y-3 grid grid-flow-row grid-cols-1 sm:grid-cols-2 ">
           <div className="font-poppins text-center cursor-pointer rounded-3xl opacity-100 hover:bg-opacity-70 transition-all duration-500 ease-in-out mt-10">
             <img
               src={Project1}
--- src/Components/UpSideCover/UpSideCover.jsx
+++ src/Components/UpSideCover/UpSideCover.jsx
@@ -3,13 +3,16 @@ import React from "react";
 
 const UpSideCover = () => {
   return (
-    <Container maxWidth="lg" className="mb-44">
-      <Box className="m-20 p-20">
+    <Container
+      maxWidth="lg"
+      className="mb-8 xs:mb-12 sm:mb-16 md:mb-24 lg:mb-44"
+    >
+      <Box className="m-4 sm:m-6 md:m-10 lg:m-20 p-8 xs:p-10 sm:p-12 md:p-16 lg:p-20">
         <Box>
-          <h1 className="text-headStrong text-[53px] font-bold capitalize tracking-wide leading-tight">
+          <h1 className="text-headStrong text-[30px] xs:text-[35px] sm:text-[40px] md:text-[45px] lg:text-[53px] font-bold capitalize tracking-wide leading-tight">
             SISINDRI SINGAMSETTI
           </h1>
-          <h1 className="text-5xl font-bold capitalize tracking-wide leading-tight">
+          <h1 className="text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold capitalize tracking-wide leading-tight">
             CODING, SOLVING &<div>INSPIRING</div>
           </h1>
         </Box>
