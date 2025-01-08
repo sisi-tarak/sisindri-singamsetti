2420a2af62d1f8fbbc49d7f61c69fd8ff64536c4
sisi_tarak <sisindrisingamsetti@gmail.com>
2025-01-08 10:22:01 +0530

    context api created ny using React.createContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import React, { Suspense, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import HomePage from "./Components/HomePage/HomePage";
-// import Contact from "./Components/Contact/Contact";
-// import Courses from "./Components/Courses/Courses";
-// import Doc from "./Components/Doc/Doc";
-// import Tutorials from "./Components/Tutorials/Tutorials";
 import About from "./Components/About/About";
 import Resume from "./Components/Resume/Resume";
 import DownSide from "./Components/DownSide/DownSide";
 import Footer from "./Components/Footer/Footer";
 import Loader from "./Components/Loader/Loader";
+import { Provider } from "./Components/Context/Context";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -22,16 +19,18 @@ const App = () => {
   return isLoading ? (
     <Loader />
   ) : (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/downside" element={<DownSide />} />
-      </Routes>
-      <Footer />
-    </Router>
+      <Provider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/downside" element={<DownSide />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </Provider>
   );
 };
 
