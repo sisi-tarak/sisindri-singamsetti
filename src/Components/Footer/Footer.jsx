ca27ba47fa16bc1ed072b561d7f39b2f072b1b14
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-01-18 20:12:49 +0530

    link created from sides to content page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,58 +4,64 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="py-12 lg:py-10 border-t border-strongBgColor">
-      <Container
-        maxWidth="lg"
-        className="grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-y-5 lg:gap-y-0"
-      >
-        <div className="text-center lg:text-left">
-          <p className="text-headColor tracking-wider text-sm xs:text-base leading-7 font-semibold uppercase mx-auto pt-5">
-            <Link to="/"> sisindri singamsetti </Link>
-          </p>
-          <p className="text-secondaryTextColor text-sm xs:text-base leading-7 font-normal mx-auto">
-            Full Stack Web Developer
-          </p>
-        </div>
-        <div className="text-center lg:text-left">
-          <p className="text-headColor tracking-wider text-sm xs:text-base leading-7 font-semibold uppercase mx-auto pt-5">
-            social media
-          </p>
-          <div className="text-secondaryTextColor text-sm xs:text-base leading-7 font-normal mx-auto no-underline">
-            <Link
-              to="https://www.linkedin.com/in/sisitarak/"
-              target="blank"
-              className="transition-all duration-500 ease-in-out hover:text-headColor"
-            >
-              LinkedIn
-            </Link>{" "}
-            | {""}
-            <Link
-              to="https://github.com/sisi-tarak"
-              target="blank"
-              className="transition-all duration-500 ease-in-out hover:text-headColor"
-            >
-              GitHub
-            </Link>{" "}
-            | {""}
-            <Link
-              to="https://www.instagram.com/sisi_tarakk/"
-              target="blank"
-              className="transition-all duration-500 ease-in-out hover:text-headColor"
-            >
-              Instagram
-            </Link>{" "}
-            | {""}
-            <Link
-              to="https://youtube.com/@singamsettisisindri/"
-              target="blank"
-              className="transition-all duration-500 ease-in-out hover:text-headColor"
-            >
-              Youtube
-            </Link>
+    <div className="flex flex-col space-y-4">
+      <div className="text-center font-Montserrat tracking-wider">
+        <Link to="/sisi">
+          Content <span className="text-textStrong">Creation</span> Page
+        </Link>
+      </div>
+      <div className="py-12 lg:py-10 border-t border-strongBgColor">
+        <Container maxWidth="lg" className="flex flex-col">
+          <div className="grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-y-5 lg:gap-y-0">
+            <div className="text-center lg:text-left">
+              <p className="text-headColor tracking-wider text-sm xs:text-base leading-7 font-semibold uppercase mx-auto pt-5">
+                <Link to="/"> sisindri singamsetti </Link>
+              </p>
+              <p className="text-secondaryTextColor text-sm xs:text-base leading-7 font-normal mx-auto">
+                Full Stack Web Developer
+              </p>
+            </div>
+            <div className="text-center lg:text-left">
+              <p className="text-headColor tracking-wider text-sm xs:text-base leading-7 font-semibold uppercase mx-auto pt-5">
+                social media
+              </p>
+              <div className="text-secondaryTextColor text-sm xs:text-base leading-7 font-normal mx-auto no-underline">
+                <Link
+                  to="https://www.linkedin.com/in/sisitarak/"
+                  target="blank"
+                  className="transition-all duration-500 ease-in-out hover:text-headColor"
+                >
+                  LinkedIn
+                </Link>{" "}
+                | {""}
+                <Link
+                  to="https://github.com/sisi-tarak"
+                  target="blank"
+                  className="transition-all duration-500 ease-in-out hover:text-headColor"
+                >
+                  GitHub
+                </Link>{" "}
+                | {""}
+                <Link
+                  to="https://www.instagram.com/sisi_tarakk/"
+                  target="blank"
+                  className="transition-all duration-500 ease-in-out hover:text-headColor"
+                >
+                  Instagram
+                </Link>{" "}
+                | {""}
+                <Link
+                  to="https://youtube.com/@singamsettisisindri/"
+                  target="blank"
+                  className="transition-all duration-500 ease-in-out hover:text-headColor"
+                >
+                  Youtube
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </div>
   );
 };
