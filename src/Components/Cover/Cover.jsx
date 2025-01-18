afb4a62fc60414d621c608430c5b0203146c3929
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-01-18 11:23:22 +0530

    sisi page error resolving in deploy

--- src/Components/Cover/Cover.jsx
+++ src/Components/Cover/Cover.jsx
@@ -1,30 +1,30 @@
-import { Container } from "@mui/material";
-import { TextRevealByScroll } from "../Animations/TextRevealByScroll";
-import { cn } from "../lib/utils";
+// import { Container } from "@mui/material";
+// import { TextRevealByScroll } from "../Animations/TextRevealByScroll";
+// import { cn } from "../lib/utils";
 
-const Cover = () => {
-  return (
-    <Container
-      maxWidth="md"
-      className="h-svh lg:h-screen w-full relative my-24"
-    >
-      <div className="flex items-center justify-center pointer-events-none">
-        <div className="w-full max-w-5xl mx-auto p-4">
-          <div
-            className={cn(
-              "rounded-lg w-full h-[500px]",
-              "bg-white/50 dark:bg-black/50 backdrop-blur-sm",
-              "flex items-center justify-center",
-              "pointer-events-auto"
-            )}
-          >
-            <TextRevealByScroll text="Magic UI will change the way you design." />
-          </div>
-        </div>
-      </div>
-      <div className="h-[200vh]" aria-hidden="true" />
-    </Container>
-  );
-};
+// const Cover = () => {
+//   return (
+//     <Container
+//       maxWidth="md"
+//       className="h-svh lg:h-screen w-full relative my-24"
+//     >
+//       <div className="flex items-center justify-center pointer-events-none">
+//         <div className="w-full max-w-5xl mx-auto p-4">
+//           <div
+//             className={cn(
+//               "rounded-lg w-full h-[500px]",
+//               "bg-white/50 dark:bg-black/50 backdrop-blur-sm",
+//               "flex items-center justify-center",
+//               "pointer-events-auto"
+//             )}
+//           >
+//             <TextRevealByScroll text="Magic UI will change the way you design." />
+//           </div>
+//         </div>
+//       </div>
+//       <div className="h-[200vh]" aria-hidden="true" />
+//     </Container>
+//   );
+// };
 
-export default Cover;
+// export default Cover;
