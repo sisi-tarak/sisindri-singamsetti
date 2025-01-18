23203fb73e2dab3cdf6e544624394586ca17ebe8
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-01-18 11:20:30 +0530

    sisi page error resolving in deploy

--- src/Components/Cover/Cover.jsx
+++ src/Components/Cover/Cover.jsx
@@ -4,7 +4,10 @@ import { cn } from "../lib/utils";
 
 const Cover = () => {
   return (
-    <Container maxWidth="md" className="h-svh lg:h-screen w-full relative my-24">
+    <Container
+      maxWidth="md"
+      className="h-svh lg:h-screen w-full relative my-24"
+    >
       <div className="flex items-center justify-center pointer-events-none">
         <div className="w-full max-w-5xl mx-auto p-4">
           <div
