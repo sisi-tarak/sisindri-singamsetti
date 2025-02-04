eaeb349f5fc6bf936b903a1e0420e55c74229c5e
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-02-04 16:58:24 +0530

    roadmap responsive updated for the frontend

--- src/Components/sisi/RoadmapPage.jsx
+++ src/Components/sisi/RoadmapPage.jsx
@@ -24,11 +24,16 @@ const RoadmapPage = () => {
                   <div className="max-w-4xl mx-auto">
                     {roadmap ? (
                       roadmaps.map((roadmap) => (
-                        <div key={roadmap.id} className="mb-10 p-6 shadow-lg">
-                          <h2 className="text-2xl font-bold mb-2">
+                        <div
+                          key={roadmap.id}
+                          className="mb-10 p-2 md:p-6 shadow-lg"
+                        >
+                          <h2 className="text-xl md:text-2xl font-bold mb-2">
                             {roadmap.title}
                           </h2>
-                          <p className="mb-6">{roadmap.description}</p>
+                          <p className="mb-6 text-sm md:text-base text-textColor/80">
+                            {roadmap.description}
+                          </p>
 
                           <div className="flex flex-col md:flex-row justify-center mx-auto gap-y-3 gap-x-5 mb-5 text-center w-full xs:w-[80%] sm:w-[60%] md:w-full">
                             {roadmap.buttons && roadmap.buttons[0] && (
@@ -56,38 +61,46 @@ const RoadmapPage = () => {
                                 ))}
                           </div>
 
-                          <h3 className="text-xl font-semibold mt-8 text-textStrong text-center">
+                          <h3 className="text-lg md:text-xl font-semibold mt-8 text-textStrong text-center">
                             Concepts:
                           </h3>
                           {Object.entries(roadmap.concepts).map(
                             ([key, topics]) => (
                               <div key={key} className="mb-4 mt-2">
-                                <h4 className="text-lg font-semibold capitalize">
+                                <h4 className="text-base md:text-lg font-semibold capitalize">
                                   {key}
                                 </h4>
                                 <ul className="list-disc pl-5">
                                   {topics.map((topic, index) => (
-                                    <li key={index}>{topic.topic}</li>
+                                    <li
+                                      key={index}
+                                      className="text-sm md:text-base"
+                                    >
+                                      {topic.topic}
+                                    </li>
                                   ))}
                                 </ul>
                               </div>
                             )
                           )}
 
-                          <h3 className="text-xl font-semibold mt-8 text-textStrong text-center">
+                          <h3 className="text-lg md:text-xl font-semibold mt-8 text-textStrong text-center">
                             Projects:
                           </h3>
                           {Object.entries(roadmap.projects).map(
                             ([level, projects]) => (
                               <div key={level} className="mb-4 mt-2">
-                                <h4 className="text-lg font-semibold capitalize">
+                                <h4 className="text-base md:text-lg font-semibold capitalize">
                                   {level} Level
                                 </h4>
                                 <ul className="list-disc pl-5">
                                   {projects.map((project, index) => (
                                     <li key={index}>
-                                      {project.name} -{" "}
-                                      <span className="italic">
+                                      <span className="text-sm md:text-base">
+                                        {project.name}
+                                      </span>{" "}
+                                      -{" "}
+                                      <span className="text-sm md:text-base text-textColor/80">
                                         {project.stack.join(", ")}
                                       </span>
                                     </li>
@@ -98,27 +111,32 @@ const RoadmapPage = () => {
                           )}
 
                           <div>
-                            <h3 className="text-xl font-semibold mt-8 text-center text-textStrong ">
+                            <h3 className="text-lg md:text-xl font-semibold mt-8 text-center text-textStrong ">
                               Interview Tips:
                             </h3>
                             <ul className="list-disc pl-5 mt-2">
                               {roadmap.interviewTips.map((tip, index) => (
-                                <li key={index}>{tip.tip}</li>
+                                <li
+                                  key={index}
+                                  className="text-sm md:text-base"
+                                >
+                                  {tip.tip}
+                                </li>
                               ))}
                             </ul>
                           </div>
 
                           <div className="mb-8">
-                            <h3 className="text-xl font-semibold mt-8 text-center text-textStrong">
+                            <h3 className="text-lg md:text-xl font-semibold mt-8 text-center text-textStrong">
                               Final Tips:
                             </h3>
                             <ul className="list-disc pl-5 mt-2">
                               {roadmap.finalTips.map((finaltip, index) => (
                                 <li key={index}>
-                                  <span className="font-medium">
+                                  <span className="font-medium text-base md:text-lg">
                                     {finaltip.title}:
                                   </span>{" "}
-                                  <span className="text-textColor/80">
+                                  <span className="text-textColor/80 text-sm md:text-base">
                                     {finaltip.description}
                                   </span>
                                 </li>
